e4bdb81f39e30338bd8fd12f443d6f2956f0fdf9
mateigraura <grauramatei@gmail.com>
2021-02-14 21:51:33 +0200

    minor fix

--- lib/services/key_service.dart
+++ lib/services/key_service.dart
@@ -6,18 +6,16 @@ Future setupKeyPair() async {
   try {
     final kpSetup = await readIsKeyPairSetup();
 
-    if (kpSetup == okMsg) {
-      return true;
-    }
+    if (kpSetup != okMsg) {
+      final kp = await generateKeyPair();
 
-    final kp = await generateKeyPair();
+      await writePrivateKey(kp.privateKey.toBase64());
 
-    await writePrivateKey(kp.privateKey.toBase64());
-
-    final pkStored = await sendPublicKey(kp.publicKey.toBase64());
-    if (!pkStored) {
-      await writePrivateKey('null');
-      await writeShouldRetry();
+      final pkStored = await sendPublicKey(kp.publicKey.toBase64());
+      if (!pkStored) {
+        await writePrivateKey('null');
+        await writeShouldRetry();
+      }
     }
   } catch (e) {
     throw e;
